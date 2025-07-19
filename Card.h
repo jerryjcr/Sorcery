01bb25209eefe22fac966297f7d7624e461d6c44
Cameron Lee <cameronkmlee10@gmail.com>
2025-07-19 14:16:19 -0400

    Remove const from getCost

--- Card.h
+++ Card.h
@@ -30,7 +30,7 @@ class Card {
                               Player& activePlayer, Player& inactivePlayer);
   virtual void describe() const = 0;
 
-  const int getCost() const { return cost; }
+  int getCost() const { return cost; }
 };
 
 #endif
