9edc7dd61eaff76fc329c9d8c4f2d34c04cc2449
Cameron Lee <cameronkmlee10@gmail.com>
2025-07-20 12:42:58 -0400

    Format ability interface

--- src/Ability.h
+++ src/Ability.h
@@ -1,18 +1,23 @@
 #ifndef ABILITY_H
 #define ABILITY_H
 
-enum TriggerType {NONE,ON_MINION_ENTER,ON_MINION_EXIT,ON_TURN_START,ON_TURN_END};
+#include "Card.h"
+#include "Player.h"
 
-class Player;
-class Card;
-
-class Ability {
-    int abilityCost;
-    public:
-        virtual void useAbility(TriggerType type, Card* target,
-                              Player& activePlayer, Player& inactivePlayer)=0;
+enum class TriggerType {
+  None,
+  StartOfTurn,
+  EndOfTurn,
+  MinionEnters,
+  MinionLeaves
 };
 
+class Ability {
+  int abilityCost;
 
+ public:
+  virtual void useAbility(TriggerType type, Card* target, Player& activePlayer,
+                          Player& inactivePlayer) = 0;
+};
 
 #endif
