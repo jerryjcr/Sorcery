d65972b0d8f227dd10d82af0765d44f894de0cc3
jerryjcr <jerry.jia295@gmail.com>
2025-07-20 14:08:41 -0400

    added banish ability

--- src/concreteAbilities.cc
+++ src/concreteAbilities.cc
@@ -1,5 +1,16 @@
 #include "concreteAbilities.h"
 
+using namespace std;
+
 void BanishAbility::useAbility(TriggerType type, Player& targetPlayer,int boardIndex){
+  if (type==TriggerType::None){
+    unique_ptr<Card> p=nullptr;
+    targetPlayer.setBoard(boardIndex, move(p));
+  }
+}
+
+void UnsummonAbility::useAbility(TriggerType type, Player& targetPlayer,int boardIndex){
+  if (type==TriggerType::None){
     
+  }
 }
