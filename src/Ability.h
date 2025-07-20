b7192bcf65fd52a98d480453e5bb55cba37be5fb
jerryjcr <jerry.jia295@gmail.com>
2025-07-20 12:34:40 -0400

    added ability interface

--- src/Ability.h
+++ src/Ability.h
@@ -1,8 +1,18 @@
-enum class TriggerType {
-  StartOfTurn,
-  EndOfTurn,
-  MinionEnters,
-  MinionLeaves
+#ifndef ABILITY_H
+#define ABILITY_H
+
+enum TriggerType {NONE,ON_MINION_ENTER,ON_MINION_EXIT,ON_TURN_START,ON_TURN_END};
+
+class Player;
+class Card;
+
+class Ability {
+    int abilityCost;
+    public:
+        virtual void useAbility(TriggerType type, Card* target,
+                              Player& activePlayer, Player& inactivePlayer)=0;
 };
 
-class Ability {};
+
+
+#endif
