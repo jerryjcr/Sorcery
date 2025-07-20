71e7d223989e2a2d72ea8cb082dca70ab8ae966e
Cameron Lee <cameronkmlee10@gmail.com>
2025-07-20 12:58:54 -0400

    Fix ritual ctor

--- src/Ritual.cc
+++ src/Ritual.cc
@@ -1,6 +1,7 @@
 #include "Ritual.h"
 
 Ritual::Ritual(const std::string& name, int cost, int charges, int chargeCost,
-         std::shared_ptr<const Ability> triggered); 
-         Card{name, cost, nullptr, triggered}, charges{charges}, chargesCost{chargesCost} {}
-
+               std::shared_ptr<const Ability> triggered)
+    : Card{name, cost, nullptr, triggered},
+      charges{charges},
+      chargeCost{chargeCost} {}
