d01c7153938468a390e584a84bbc36b6ae36f287
Cameron Lee <cameronkmlee10@gmail.com>
2025-07-20 12:24:12 -0400

    Fix Card.cc

--- src/Card.cc
+++ src/Card.cc
@@ -1,16 +1,21 @@
-#include Card.h
+#include "Card.h"
 
-Card::Card( const std::string& name, int cost,
-            std::shared_ptr<const Ability> activated = nullptr,
-            std::shared_ptr<const Ability> triggered = nullptr):
-            name{name}, cost{cost}, activatedAbility{activated},
-            triggeredAbility{triggered} {}
-
-int Card::getCost() {
-    return cost;
-}
+Card::Card(const std::string& name, int cost,
+           std::shared_ptr<const Ability> activated,
+           std::shared_ptr<const Ability> triggered)
+    : name{name},
+      cost{cost},
+      activatedAbility{activated},
+      triggeredAbility{triggered} {}
 
 void Card::activateAbility(Card* target, Player& activePlayer,
-                               Player& inactivePlayer) {
-    // figure this out later
+                           Player& inactivePlayer) {
+  // figure this out later
 }
+
+void Card::triggerAbility(TriggerType type, Card* target, Player& activePlayer,
+                          Player& inactivePlayer) {
+  // figure this out later
+}
+
+int Card::getCost() const { return cost; }
