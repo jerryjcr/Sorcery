6a3be9d6c1bf0a43bf32e6c2c57c97598b0f47ea
adrian kwok <icantthinkgoodusername@gmail.com>
2025-07-21 16:06:11 -0400

    Created basic game loop

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -9,6 +10,8 @@
 #include "Minion.h"
 #include "Player.h"
 #include "Ritual.h"
+
+const int kStartOfTurnMagic = 1;
 
 int main(int argc, char* argv[]) {
   // processing command line args
@@ -66,8 +69,106 @@ int main(int argc, char* argv[]) {
   Player p1{name1, std::move(deck1)};
   Player p2{name2, std::move(deck2)};
 
-  // main loop
+  Player& activePlayer = p1;
+  Player& opponentPlayer = p2;
   while (true) {
-    // due later do later
+    // start of turn
+
+    // gain 1 magic
+    activePlayer.adjustMagic(kStartOfTurnMagic);
+    // draw a card
+    activePlayer.drawCard();
+
+    // start of turn effects trigger
+    // do this later
+
+    // "action phase"
+    while (true) {
+      // have to change the way that input works with the -init command
+      std::string cmd;
+      std::getline(std::cin, cmd);
+      std::stringstream currline{cmd};
+      cmd.clear();
+      currline >> cmd;
+
+      if (cmd == "help") {
+        // help message goes here do if you are bored
+
+      } else if (cmd == "end") {
+        break;
+
+      } else if (cmd == "quit") {
+        return 0;
+
+      } else if (cmd == "draw" && testingMode) {
+        activePlayer.drawCard();
+
+      } else if (cmd == "discard" && testingMode) {
+        int handInd;
+        if (currline >> handInd) {
+          activePlayer.discard(handInd);
+        } else {
+          // some sort of error since no index given
+        }
+
+      } else if (cmd == "attack") {
+        int myMinion;
+        int target;
+        if (currline >> myMinion) {
+          // player checks if myMinion is out of range
+          if (currline >> target) {
+            activePlayer.attackMinion(myMinion, opponentPlayer, target);
+          } else {  // todo: check for invalid input versus EOF
+            // attacking the other player
+            activePlayer.attackPlayer(myMinion, opponentPlayer);
+          }
+        } else {
+          // bruh error moment
+        }
+
+      } else if (cmd == "play") {
+        int myCard;
+        int targetPlayer;
+        char targetCard;
+        if (currline >> myCard) {
+          if (currline >> targetPlayer && currline >> targetCard) {
+            if ((targetPlayer != 1 && targetPlayer != 2) ||
+                (targetCard != 'r' && (targetCard < '1' || targetCard > '5'))) {
+              // error, bad input
+            } else {
+              int targetInd;
+              // we all agree that 0 is how you target a ritual
+              targetCard == 'r' ? targetInd = 0 : targetInd = targetCard - '0';
+
+              // this is weird since the players may forget who player 1 and 2
+              // are, perhaps send a message reminding them?
+              if (targetPlayer == 1) {
+                activePlayer.playCard(myCard, p1, targetCard);
+              } else {  // must be targeting player 2
+                activePlayer.playCard(myCard, p2, targetCard);
+              }
+            }
+          } else {  // todo: check for invalid input versus EOF
+            activePlayer.playCard(myCard);
+          }
+        } else {
+          // error no card given
+        }
+
+      } else if (cmd == "use") {
+        // do later since abilities aren't done yet
+      } else if (cmd == "describe") {
+        // todo
+      } else if (cmd == "hand") {
+        // do later
+      } else if (cmd == "board") {
+        // do later
+      }
+    }
+
+    // end of turn
+    // activate end of turn triggers (do later)
+    //...
+    std::swap(activePlayer, opponentPlayer);
   }
 }
