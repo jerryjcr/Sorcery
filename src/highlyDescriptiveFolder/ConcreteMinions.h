3dea5d746bc0d0304977c5d4d0070f9bd4f4d581
jerryjcr <jerry.jia295@gmail.com>
2025-07-22 18:47:14 -0400

    abilities now return bool

--- src/highlyDescriptiveFolder/ConcreteMinions.h
+++ src/highlyDescriptiveFolder/ConcreteMinions.h
@@ -22,7 +22,7 @@ class EarthElemental : public Minion {
   public:
     EarthElemental();
     ~EarthElemental() = default;
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class BoneGolem : public Minion {
@@ -31,44 +31,42 @@ class BoneGolem : public Minion {
     ~BoneGolem() = default;
     virtual void triggerAbility(TriggerType type, Player& targetPlayer,
                               int boardIndex) override;//override in order to ensure target is itself
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class FireElemental : public Minion {
   public:
     FireElemental();
     ~FireElemental() = default;
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class PotionSeller : public Minion {
   public:
     PotionSeller();
     ~PotionSeller() = default;
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class NovicePyromancer : public Minion {
   public:
     NovicePyromancer();
     ~NovicePyromancer() = default;
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class ApprenticeSummoner : public Minion {
   public:
     ApprenticeSummoner();
     ~ApprenticeSummoner() = default;
-    virtual void activateAbility(Player& targetPlayer, int boardIndex) override;//override for error checking
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 class MasterSummoner : public Minion {
   public:
     MasterSummoner();
     ~MasterSummoner() = default;
-    virtual void activateAbility(Player& targetPlayer, int boardIndex) override;//override for error checking
-    virtual void describe() const override;
+    virtual const std::vector<std::string>& getDescription() const override;
 };
 
 #endif
