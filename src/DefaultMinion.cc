2eb08b14d4881415795bf5f928061b17fd6ce2f3
Cameron Lee <cameronkmlee10@gmail.com>
2025-07-21 23:58:23 -0400

    Variety of changes to allow code to compile with default minions

---
+++ src/DefaultMinion.cc
@@ -0,0 +1,6 @@
+#include "DefaultMinion.h"
+
+#include "Card.h"
+#include "Minion.h"
+
+DefaultMinion::DefaultMinion() : Minion{"Default", 1, 1, 1} {}
---
+++ src/DefaultMinion.h
@@ -0,0 +1,14 @@
+#ifndef DEFAULTMINION_H_
+#define DEFAULTMINION_H_
+
+#include "Card.h"
+#include "Minion.h"
+
+// for testing
+class DefaultMinion : public Minion {
+ public:
+  DefaultMinion();
+  ~DefaultMinion() = default;
+};
+
+#endif
