276d08320552d7768c86f5aeb616bca7a00765f3
adrian kwok <icantthinkgoodusername@gmail.com>
2025-07-22 17:23:18 -0400

    formatted

--- src/DefaultMinion.cc
+++ src/DefaultMinion.cc
@@ -5,5 +5,6 @@
 
 DefaultMinion::DefaultMinion() : Minion{"Default", 1, 1, 1, nullptr} {}
 const std::vector<const std::string>& DefaultMinion::getDescription() const {
-    return std::vector<const std::string> {"this is the default minion", "I really hope this works",""};
+  return std::vector<const std::string>{"this is the default minion",
+                                        "I really hope this works", ""};
 }
