a9defdfebf6e61caaf6c977b28cb9c86be61ec39
jerryjcr <jerry.jia295@gmail.com>
2025-07-22 16:38:35 -0400

    FINAL CONCRETEABILITIES.H PLEASE

--- src/highlyDescriptiveFolder/ConcreteAbilities.h
+++ src/highlyDescriptiveFolder/ConcreteAbilities.h
@@ -6,116 +6,124 @@
 class BanishAbility : public Ability {
   public:
     BanishAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 class UnsummonAbility : public Ability {
   public:
     UnsummonAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 class RechargeAbility : public Ability {
   public:
     RechargeAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 class DisenchantAbility : public Ability {
   public:
     DisenchantAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 class RaiseDeadAbility : public Ability {
   public:
     RaiseDeadAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //only effects the targetted player, so it must be called twice for the intended effect
 class BlizzardAbility : public Ability {
   public:
     BlizzardAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //adds 1/1 to target minion on minion leave
 class BoneGolemAbility : public Ability {
   public:
     BoneGolemAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //deal 1 damage on minion enter
 class FireElementalAbility : public Ability {
   public:
     FireElementalAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //+0/1 to all your minions on turn end
 class PotionSellerAbility : public Ability {
   public:
     PotionSellerAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //1 damage to target minion
 class NovicePyromancerAbility : public Ability {
   public:
     NovicePyromancerAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //summon a 1/1 air elemental, cannot be used if board is full
 class ApprenticeSummonerAbility : public Ability {
   public:
     ApprenticeSummonerAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //summon up to 3 air elementals, cannot be used if board is full
 class MasterSummonerAbility : public Ability {
   public:
     MasterSummonerAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //gain 1 magic on turn start
 class DarkRitualAbility : public Ability {
   public:
     DarkRitualAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& activePlayer, Player& inactivePlayer,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //give +1/1 on minion enter
 class AuraOfPowerAbility : public Ability {
   public:
     AuraOfPowerAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 //destoys minion on enter
 class StandstillAbility : public Ability {
   public:
     StandstillAbility(int cost);
-    virtual void useAbility(TriggerType type, Player& targetPlayer,
-                          Player& nontargetPlayer, std::unique_ptr<Card> target) override;
+    virtual void useAbility(Player& targetPlayer,
+                              std::unique_ptr<Card>& targetCard,
+                              TriggerType type = TriggerType::None) override;
 };
 
 #endif
