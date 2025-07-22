1611d3883232ac2e381917ba4b4ca22ddbe1b18a
jerryjcr <jerry.jia295@gmail.com>
2025-07-22 12:39:18 -0400

    unreverted :P

--- src/Ability.h
+++ src/Ability.h
@@ -14,10 +14,22 @@ enum class TriggerType {
   MinionLeaves
 };
 
+enum class TargetType { Minion, Ritual };
+
 class Ability {
+  std::string name;
+  int cost;
+  std::vector<TargetType> validTargets;
+
  public:
+  Ability(const std::string& name, int cost,
+          std::vector<TargetType> validTargets = {});
+
   virtual void useAbility(TriggerType type, Player& targetPlayer,
                           int boardIndex) = 0;
+
+  bool requiresTarget() const;
+  bool canTarget(TargetType targetType) const;
 };
 
 #endif
