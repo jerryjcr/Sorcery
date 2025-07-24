5dc7c48d82592702a05c6484de785d16e8f98c66
jerryjcr <jerry.jia295@gmail.com>
2025-07-24 19:41:25 -0400

    changed main use params

--- src/Ability.h
+++ src/Ability.h
@@ -32,7 +32,7 @@ class Ability {
                           TriggerType type = TriggerType::None);
   // target
   virtual bool useAbility(Player& targetPlayer, Card& targetCard,
-                          Player& otherPlayer,
+                          Player& activePlayer, Player& otherPlayer,
                           TriggerType type = TriggerType::None);
 
   bool requiresTarget() const;
--- src/main.cc
+++ src/main.cc
@@ -271,15 +271,15 @@ int main(int argc, char* argv[]) {
                 // are, perhaps send a message reminding them?
                 if (targetPlayer == 1) {
                   if (cmd == "play") {
-                    activePlayer->playCard(myCard, p1, targetInd, p2, isRitual);
+                    activePlayer->playCard(myCard, p1, targetInd, *activePlayer, *opponentPlayer, isRitual);
                   } else {
-                    activePlayer->use(myCard, p1, targetInd, p2, isRitual);
+                    activePlayer->use(myCard, p1, targetInd, *activePlayer, *opponentPlayer, isRitual);
                   }
                 } else {  // must be targeting player 2
                   if (cmd == "play")
-                    activePlayer->playCard(myCard, p2, targetInd, p1, isRitual);
+                    activePlayer->playCard(myCard, p2, targetInd, *activePlayer, *opponentPlayer, isRitual);
                   else
-                    activePlayer->use(myCard, p2, targetInd, p1, isRitual);
+                    activePlayer->use(myCard, p2, targetInd, *activePlayer, *opponentPlayer, isRitual);
                 }
               }
             } else {
